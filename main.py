11c854ae84a98af568f97a3ed741f29d4f120e43
Muchammad Ibnu <13518072@std.stei.itb.ac.id>
2021-03-01 22:58:02 +0700

    resolve error

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from activation import activationFunction
 
 if __name__ == "__main__":
     act = activationFunction.Activation(vector=[1, 1, 1])
-    print(act.softmax())
+    print(act.sigmoid())
+    print()
