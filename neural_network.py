6114d77ad6d305a58f93463bf2b2d470fd29f32f
mdaffad <13518141@std.stei.itb.ac.id>
2021-03-02 07:12:42 +0700

    create driver test

--- neural_network.py
+++ neural_network.py
@@ -40,20 +40,25 @@ class Layer(InputLayer):
     def activate(self):
         self.result = self.activation_function(self.result)
     
-    def compute(self):
+    def sigma(self):
         self.result = np.matmul(np.transpose(self.input_value), self.weight) + self.bias
     
-    def output(self):
-        self.compute()
-        self.result()
+    def compute(self):
+        self.sigma()
+        self.activate()
 
 # driver test
-def main():
-    input_layer = InputLayer([0,1,2])
-    print(type(input_layer.input_value))    
-    matrix = np.array([[0,1,2], [3,4,5]])
-    print(matrix[0])
-    print(np.matmul(matrix, input_layer.input_value))    
+def main():  
+    layer = []
+    layer.append(InputLayer([0.05, 0.1]))
+    import math
+    layer.append(Layer([[0.15, 0.2], [0.25, 0.3]], [0.35,0.35], lambda x:1/(1 + math.e**(-1 * x)))) # result from slide [0.5933, 0.5969] 
+    layer.append(Layer([[0.4, 0.45], [0.5, 0.55]], [0.6,0.6], lambda x:1/(1 + math.e**(-1 * x)))) # result from slide [0.7514, 0.7729]
+    neural_network = NeuralNetwork()
+    neural_network.base_layer = layer
+    neural_network.solve()
+    for x in neural_network.current_layer:
+        print(x.result)
 
 if __name__ == "__main__":
     main()
