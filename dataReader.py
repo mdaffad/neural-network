e6252174a0b70925dcf35eba712d77ff6443a05b
Daffa Pratama <13518033@std.stei.itb.ac.id>
2021-03-03 23:15:40 +0700

    data training reader

--- dataReader.py
+++ dataReader.py
@@ -1,17 +1,34 @@
 import csv
+import pandas as pd
 
-with open("xor-data.csv", newline="") as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(row["x0"], row["x1"], row["x2"], row["f"])
 
-with open("xor-weight.csv", newline="") as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(
-            row["layerFrom"],
-            row["from"],
-            row["layerTo"],
-            row["to"],
-            row["weight"],
-        )
+def readData(filename="xor-data.csv"):
+    data = []
+    target_class = []
+    with open(filename, newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append([int(row["x1"]), int(row["x2"])])
+            target_class.append(int(row["f"]))
+    return data, target_class
+
+
+def readWeight(filename="xor-weight.csv"):
+    # with open("xor-weight.csv", newline="") as csvfile:
+    #     reader = csv.DictReader(csvfile)
+    #     for row in reader:
+    #         print(
+    #             row["layerFrom"],
+    #             row["from"],
+    #             row["layerTo"],
+    #             row["to"],
+    #             row["weight"],
+    #         )
+    f = open("test.txt", "r")
+    f1 = f.readlines()
+    activation = []
+    bias = []
+    weight = []
+    for x in f1:
+        splitted = x.strip('\n').split(' ')
+        print(splitted)
