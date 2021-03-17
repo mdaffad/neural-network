71b11a62e1f62d0107598715ca19f36054dc44b2
mdaffad <13518141@std.stei.itb.ac.id>
2021-03-17 14:26:21 +0700

    init back propagation

--- main.py
+++
@@ -1,6 +0,0 @@
-from activation import activationFunction
-
-if __name__ == "__main__":
-    act = activationFunction.Activation(vector=[1, 1, 1])
-    print(act.sigmoid())
-    print()
--- neural_network.py
+++ neural_network.py
@@ -32,9 +32,56 @@ class NeuralNetwork:
                 self.current_layer[idx].input_value = self.current_layer[idx-1].result
             else:
                 print("Input layer \t:", self.current_layer[idx].input_value)
-                # print(self.current_layer[idx].input_value)
             self.current_layer[idx].compute()
 
+    def draw(self):
+        from graphviz import Digraph
+        f = Digraph('Feed Forward Neural Network', filename='ann1.gv')
+        f.attr('node', shape='circle', fixedsize='true', width='0.9')
+
+        for i in range(len(self.current_layer)):
+            if i != 0:
+                if i == 1:
+                    print(self.current_layer[i].weight)
+                    print(self.current_layer[i].bias)
+                    for j in range(len(self.current_layer[i].weight)):
+                        # f.edge("x{i}","hidden{i}")
+                        for k in range(len(self.current_layer[i].weight[j])):
+                            print(f'x{j}')
+                            print(self.current_layer[i].weight[j][k])
+                            print(f'h{i}_{k}')
+                            f.edge(f'x{j}', f'h{i}_{k}', str(
+                                self.current_layer[i].weight[j][k]))
+                    for j in range(len(self.current_layer[i].bias)):
+                        print(f'bx')
+                        print(self.current_layer[i].bias[j])
+                        print(f'h{i}_{j}')
+                        f.edge(f'bx', f'h{i}_{j}', str(
+                            self.current_layer[i].bias[j]))
+                else:
+                    print(self.current_layer[i].weight)
+                    for j in range(len(self.current_layer[i].weight)):
+                        for k in range(len(self.current_layer[i].weight[j])):
+                            print(f'h{i-1}_{j}')
+                            print(self.current_layer[i].weight[j][k])
+                            print(f'h{i}_{k}')
+                            f.edge(f'h{i-1}_{j}', f'h{i}_{k}',
+                                str(self.current_layer[i].weight[j][k]))
+                    for j in range(len(self.current_layer[i].bias)):
+                        print(f'bhx{i-1}')
+                        print(self.current_layer[i].bias[j])
+                        print(f'h{i}_{j}')
+                        f.edge(f'bhx{i-1}', f'h{i}_{j}',
+                            str(self.current_layer[i].bias[j]))
+
+        f.view()
+    
+
+    def back_propagation(self):
+        for layer in self.current_layer:
+            layer.weight = layer.update_weight()
+            layer.bias = layer.update_bias()
+        # pass
 
 class InputLayer:
     def __init__(self, arr=[]):
@@ -62,31 +109,22 @@ class Layer(InputLayer):
         if(len(self.weight[0]) == 1):
             self.result = np.matmul(
                 self.input_value, self.weight.flatten()) + self.bias
-            # print("masuk 1")
-
         else:
             self.result = np.matmul(self.input_value, self.weight) + self.bias
         print("Sigma \t: ", self.result)
-        # print(self.result)
 
     def compute(self):
         print("Input \t: ", self.input_value)
-        # print(self.input_value)
         self.sigma()
         self.activate()
-
         print("Weight \t: ", self.weight)
-        # print(self.weight)
         print("Result \t: ", self.result)
-        # print(self.result)
-        # print("transpose")
-        # print(np.transpose(
-        #     self.input_value))
-        # print("weight")
-
-        # print(self.weight)
-        # print("result")
-        # print(self.result)
+    
+    def update_weight(self):
+        pass
+    
+    def update_bias(self):
+        pass
 # driver test
 
 
@@ -114,8 +152,6 @@ def main():
         print(activation[i], end=" ")
         layer.append(Layer(weight[i], bias[i], act, threshold=0.1))
     print("")
-    # layer.append(Layer([[20, -20], [20, -20]], [-10, 30], sigmoid, threshold=0.1))
-    # layer.append(Layer([[20, 20]], [-30], sigmoid,  threshold=0.1))
     for data in data_training:
         layer.insert(0, InputLayer(data))
         neural_network.base_layer = layer
@@ -129,47 +165,8 @@ def main():
         print("Result : Good Predict")
     else:
         print("Result : Wrong Predict")
-    from graphviz import Digraph
-    f = Digraph('Feed Forward Neural Network', filename='ann1.gv')
-    f.attr('node', shape='circle', fixedsize='true', width='0.9')
-
-    for i in range(len(neural_network.current_layer)):
-        if i != 0:
-            if i == 1:
-                print(neural_network.current_layer[i].weight)
-                print(neural_network.current_layer[i].bias)
-                for j in range(len(neural_network.current_layer[i].weight)):
-                    # f.edge("x{i}","hidden{i}")
-                    for k in range(len(neural_network.current_layer[i].weight[j])):
-                        print(f'x{j}')
-                        print(neural_network.current_layer[i].weight[j][k])
-                        print(f'h{i}_{k}')
-                        f.edge(f'x{j}', f'h{i}_{k}', str(
-                            neural_network.current_layer[i].weight[j][k]))
-                for j in range(len(neural_network.current_layer[i].bias)):
-                    print(f'bx')
-                    print(neural_network.current_layer[i].bias[j])
-                    print(f'h{i}_{j}')
-                    f.edge(f'bx', f'h{i}_{j}', str(
-                        neural_network.current_layer[i].bias[j]))
-            else:
-                print(neural_network.current_layer[i].weight)
-                for j in range(len(neural_network.current_layer[i].weight)):
-                    for k in range(len(neural_network.current_layer[i].weight[j])):
-                        print(f'h{i-1}_{j}')
-                        print(neural_network.current_layer[i].weight[j][k])
-                        print(f'h{i}_{k}')
-                        f.edge(f'h{i-1}_{j}', f'h{i}_{k}',
-                               str(neural_network.current_layer[i].weight[j][k]))
-                for j in range(len(neural_network.current_layer[i].bias)):
-                    print(f'bhx{i-1}')
-                    print(neural_network.current_layer[i].bias[j])
-                    print(f'h{i}_{j}')
-                    f.edge(f'bhx{i-1}', f'h{i}_{j}',
-                           str(neural_network.current_layer[i].bias[j]))
-
-    f.view()
-
+    
+    neural_network.draw()
 
 if __name__ == "__main__":
     main()
