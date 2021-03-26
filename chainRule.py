c1728f414abae5161570a84fdb9d93df90a680e0
mdaffad <13518141@std.stei.itb.ac.id>
2021-03-26 16:03:06 +0700

    check arr_target for implement gradient batch done

--- chainRule.py
+++ chainRule.py
@@ -13,7 +13,7 @@ def chainRuleHidden (arr_target, arr_out_o, arr_hiddenLayer_weight, out_h, vecto
         arr = []
         output = chainRuleOutput1(arr_target[j], arr_out_o[j])
         arr.append(output)
-        print("target : ",arr_target)
+        # print("target : ",arr_target)
         # print(arr_hiddenLayer_weight[j])
         result = np.prod(arr) * arr_hiddenLayer_weight[j]
         sum_Output += result
