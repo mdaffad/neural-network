fd7cdea326bbd0bb2dcea4805a78ea5cfe0cff77
Daffa Pratama <13518033@std.stei.itb.ac.id>
2021-03-27 18:58:44 +0700

    add param

--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,4 @@
+from chainRule import *
 import numpy as np
 import math
 from activation.activationFunction import linear, sigmoid, relu, softmax
@@ -13,7 +14,7 @@ relu = np.vectorize(relu)
 
 
 class NeuralNetwork:
-    def __init__(self, base_layer, learning_rate = 0.001, error_threshold = 0.001, max_iter = 100, batch_size = 1):
+    def __init__(self, base_layer, learning_rate=0.001, error_threshold=0.001, max_iter=100, batch_size=1):
         self.base_layer = base_layer
         self.current_layer = base_layer.copy()
         self.learning_rate = learning_rate
@@ -73,18 +74,18 @@ class NeuralNetwork:
                             # print(self.current_layer[i].weight[j][k])
                             # print(f'h{i}_{k}')
                             f.edge(f'h{i-1}_{j}', f'h{i}_{k}',
-                                str(self.current_layer[i].weight[j][k]))
+                                   str(self.current_layer[i].weight[j][k]))
                     for j in range(len(self.current_layer[i].bias)):
                         # print(f'bhx{i-1}')
                         # print(self.current_layer[i].bias[j])
                         # print(f'h{i}_{j}')
                         f.edge(f'bhx{i-1}', f'h{i}_{j}',
-                            str(self.current_layer[i].bias[j]))
+                               str(self.current_layer[i].bias[j]))
 
         f.view()
 
     def learn(self, data):
-         # placeholder
+        # placeholder
         current_iter = 0
         target = []
         result = []
@@ -93,14 +94,16 @@ class NeuralNetwork:
             error = 0.0
             for index, item in data.iterrows():
                 # Prepare input
-                self.enqueue_layer(InputLayer([item['sepal_length'], item['sepal_width'], item['petal_length'], item['petal_width']]))
+                self.enqueue_layer(InputLayer(
+                    [item['sepal_length'], item['sepal_width'], item['petal_length'], item['petal_width']]))
                 # Forward andd result
                 self.forward_propagation()
-                
-                target.append([item['Class_1'], item['Class_2'], item['Class_3']])
+
+                target.append(
+                    [item['Class_1'], item['Class_2'], item['Class_3']])
                 print("Target : ", target)
                 result.append(self.current_layer[-1].result)
-                if self.current_layer[-1].activation_function_name == "relu" or self.current_layer[-1].activation_function_name == "sigmoid"  or self.current_layer[-1].activation_function_name == "linear":
+                if self.current_layer[-1].activation_function_name == "relu" or self.current_layer[-1].activation_function_name == "sigmoid" or self.current_layer[-1].activation_function_name == "linear":
                     error += lossFunction(target, result, 3)
                 elif self.current_layer[-1].activation_function_name == "softmax":
                     for i in range(len(target)):
@@ -110,7 +113,7 @@ class NeuralNetwork:
                                 error += lossSoftmax(result[i][j])
                 print("error : ", error)
                 if error < self.error_threshold:
-                    break 
+                    break
 
                 # cleaning layer
                 self.deque_layer()
@@ -123,22 +126,23 @@ class NeuralNetwork:
                     target.clear()
                     result.clear()
                     error = 0
-            
+
             if current_iter < self.max_iter:
                 break
+
     def back_propagation(self, arr_target, arr_out):
         for i in range(len(self.current_layer) - 1, -1, -1):
-            if i != len(self.current_layer) - 2 and i > 0: # Not input or output layer
+            if i != len(self.current_layer) - 2 and i > 0:  # Not input or output layer
                 for j in range(len(self.current_layer[i].weight)):
                     for k in range(len(self.current_layer[i].weight[j])):
-                        self.current_layer[i].weight[j][k] = self.current_layer[i].update_weight(arr_target, arr_out, 
-                        self.current_layer[i].weight[j], self.current_layer[i].result[j], self.current_layer[i].input_value[j], self.learning_rate)
+                        self.current_layer[i].weight[j][k] = self.current_layer[i].update_weight(arr_target, arr_out,
+                                                                                                 self.current_layer[i].weight[j], self.current_layer[i].result[j], self.current_layer[i].input_value[j], self.learning_rate)
                 for j in range(len(self.current_layer[i].bias)):
-                    self.current_layer[i].bias = self.current_layer[i].update_bias(arr_target, arr_out, 
-                    self.current_layer[i].bias, self.current_layer[i].result[j], np.array([1 for x in range(len(self.current_layer[i].bias))]), self.learning_rate)
+                    self.current_layer[i].bias = self.current_layer[i].update_bias(arr_target, arr_out,
+                                                                                   self.current_layer[i].bias, self.current_layer[i].result[j], np.array([1 for x in range(len(self.current_layer[i].bias))]), self.learning_rate)
             elif i == len(self.current_layer):
-                self.current_layer[i].weight = self.current_layer[i].update_weight_output(arr_target, arr_out, 
-                        self.current_layer[i].weight, self.current_layer[i].result[j], self.current_layer[i].input_value[j], self.learning_rate)
+                self.current_layer[i].weight = self.current_layer[i].update_weight_output(arr_target, arr_out,
+                                                                                          self.current_layer[i].weight, self.current_layer[i].result[j], self.current_layer[i].input_value[j], self.learning_rate)
 
     def predict(self, data):
         result = []
@@ -147,20 +151,23 @@ class NeuralNetwork:
         total_data = len(data.index)
         for index, item in data.iterrows():
             # Prepare input
-            self.enqueue_layer(InputLayer([item['sepal_length'], item['sepal_width'], item['petal_length'], item['petal_width']]))
-            
+            self.enqueue_layer(InputLayer(
+                [item['sepal_length'], item['sepal_width'], item['petal_length'], item['petal_width']]))
+
             # Forward andd result
             self.forward_propagation()
             target.append([item['Class_1'], item['Class_2'], item['Class_3']])
             result.append(self.current_layer[-1].result)
             max_index_col_result = np.argmax(result[-1], axis=0)
             max_index_col_data = np.argmax(target[-1], axis=0)
-            if(max_index_col_data == max_index_col_result): precise = precise + 1
+            if(max_index_col_data == max_index_col_result):
+                precise = precise + 1
             self.deque_layer()
         accuracy = 0.0
-        accuracy = float (precise / total_data)
+        accuracy = float(precise / total_data)
         print("Accuracy \t: ", accuracy)
-             
+
+
 class InputLayer:
     def __init__(self, arr=[]):
         self.input_value = np.array(arr)
@@ -169,12 +176,14 @@ class InputLayer:
     def compute(self):
         pass
 
-from chainRule import *
+
 class Layer(InputLayer):
     def __init__(self, neuron_input, neuron_output, activation_function, activation_function_name, **kwargs):
         super().__init__([])
-        self.weight = np.array([[1.5 * (1.0 - random.random()) for x in range(neuron_output)] for j in range(neuron_input)])
-        self.bias = np.array([1.5 * (1.0 - random.random()) for x in range(neuron_output)])
+        self.weight = np.array([[1.5 * (1.0 - random.random())
+                                 for x in range(neuron_output)] for j in range(neuron_input)])
+        self.bias = np.array([1.5 * (1.0 - random.random())
+                              for x in range(neuron_output)])
         self.result = np.array([])
         self.activation_function = activation_function
         self.activation_function_name = activation_function_name
@@ -198,18 +207,17 @@ class Layer(InputLayer):
         self.activate()
         print("Weight \t: ", self.weight)
         print("Result \t: ", self.result)
-    
+
     def update_weight(self, arr_target, arr_out_o, arr_hiddenLayer_weight, out_h, vector_i, learning_rate):
         return chainRuleHidden(arr_target, arr_out_o, arr_hiddenLayer_weight, out_h, vector_i, self.activation_function_name) * learning_rate * -1
 
-    
     def update_weight_output(self, arr_target, arr_out_o, arr_hiddenLayer_weight, out_h, vector_i, learning_rate):
         return chainRuleOutput2(arr_target, arr_out_o, arr_hiddenLayer_weight, out_h, vector_i, self.activation_function_name) * learning_rate * -1
 
-
     def update_bias(self, arr_target, arr_out_o, arr_hiddenLayer_weight, out_h, vector_i, learning_rate):
         return chainRuleHidden(arr_target, arr_out_o, arr_hiddenLayer_weight, out_h, vector_i, self.activation_function_name) * learning_rate * -1
-    
+
+
 class OutputLayer(Layer):
     def __init__(self, neuron, activation_function, **kwargs):
         super().__init__(neuron, activation_function, **kwargs)
@@ -219,8 +227,8 @@ class OutputLayer(Layer):
 
 
 def main():
-    print('Feed Forward Neural Network : XOR')
-    print("=================================")
+    # print('Feed Forward Neural Network : XOR')
+    # print("=================================")
 
     parameter = read_parameter()
     data = read_data()
@@ -243,16 +251,23 @@ def main():
             act = softmax
 
         # Case for near Input Layer or the Output Layer
-        if index == 0: layer.append(Layer(NEURON_INPUT, item['neuron'], act, item['activation'], threshold=0.1))
-        elif index > 0 and index != len(model.index): layer.append(Layer(model.iloc[index - 1, 0], item['neuron'], act, item['activation'], threshold=0.1))
-        elif index == model.index: layer.append(OutputLayer(model.iloc[index - 1, 0], item['neuron'], act, item['activation'], threshold=0.1))
-    
+        if index == 0:
+            layer.append(
+                Layer(NEURON_INPUT, item['neuron'], act, item['activation'], threshold=0.1))
+        elif index > 0 and index != len(model.index):
+            layer.append(Layer(
+                model.iloc[index - 1, 0], item['neuron'], act, item['activation'], threshold=0.1))
+        elif index == model.index:
+            layer.append(OutputLayer(
+                model.iloc[index - 1, 0], item['neuron'], act, item['activation'], threshold=0.1))
+
     print("")
 
     # Build ANN model from layer and learn process
-    neural_network = NeuralNetwork(layer, learning_rate, error_threshold, max_iter, batch_size)
+    neural_network = NeuralNetwork(
+        layer, learning_rate, error_threshold, max_iter, batch_size)
     neural_network.learn(data)
-    
+
     # print("Target Class \t: ", target)
     # print("Predict Class \t: ", result)
     # print("=================================")
@@ -262,6 +277,7 @@ def main():
     #     print("Result : Wrong Predict")
     neural_network.predict(data)
     neural_network.draw()
+
 
 if __name__ == "__main__":
     main()
